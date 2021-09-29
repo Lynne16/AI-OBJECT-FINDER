09f6aab4df86e5871b81de6d1622934b216f27a4
Lynne16 <76992610+Lynne16@users.noreply.github.com>
2021-09-29 11:08:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 status="";
-
+objects=[];
 function preload(){
 }
 
@@ -12,6 +12,31 @@ video.hide();
 
 function draw(){
     image(video,0,0,450,400);
+
+    if(status != ""){
+
+        for(var i=0; i < objects.length; i++){
+            fill('skyblue');
+            percent =floor(objects[i].confidence  * 100);
+            text(objects[i].label+" "+percent+"%",objects[i].x,objects[i].y);
+            noFill();
+            stroke('skyblue');
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+
+            if(objects[i].label == input){
+                video.stop();
+                objectDetector.detect(gotResults);
+                document.getElementById("info"),innerHTML="Status: "+input+" found";
+                synth=window.speechSynthesis;
+                var utterThis=new SpeechSynthesisUtterance(input+" found")
+                synth.speak(utterThis);
+            }
+            else{
+                document.getElementById("info").innerHTML=input+" not found";
+            }
+        }
+    }
+
 }
 
 function start(){
@@ -23,4 +48,14 @@ function start(){
 function modelLoaded(){
     console.log('Model Loaded!!!');
     status=true;
+}
+
+function gotResults(){
+    if(error){
+    console.error(error)
+    }
+    else{
+        console.log(results);
+        objects=results;
+    }
 }
